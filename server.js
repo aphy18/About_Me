1777024b0c9c089669bf1b67a89204a5b7e33528
aphy18 <aphason1@gmail.com>
2022-06-23 15:44:04 -0400

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-const path = require('path')
+const port = process.env.PORT || 8080;
+// const path = require('path')
 
 app.set('view engine', 'ejs');
 
